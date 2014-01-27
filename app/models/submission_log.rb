da9f35173c3e7cd1c9ccb75e0dd3a5b1c7042bbf
Boushey, Geoffrey <Geoffrey.Boushey@ucsf.edu>
2014-01-27 09:35:57 -0800

    first commit

---
+++ app/models/submission_log.rb
@@ -0,0 +1,21 @@
+class SubmissionLog < ActiveRecord::Base
+  belongs_to :record
+  has_many :uploadArchives
+  
+  attr_accessible :archiveresponse
+  
+  def filtered_response
+    
+    if !self.archiveresponse.nil?
+  
+      if self.archiveresponse.include?("QUEUED") || self.archiveresponse.include?("PENDING")
+        return "Success: Record Uploaded to Datashare"
+      else
+        return "Failed"# + self.archiveresponse
+      end
+    
+    end
+    
+  end
+  
+end
---
+++ test/integration/uploads_integration_test.rb
@@ -0,0 +1,81 @@
+require 'test_helper'
+require "capybara/rails"
+
+class Uploads_integration_Test < ActiveSupport::TestCase
+  
+  include Capybara::DSL
+  
+  setup do
+    FileUtils.rm_rf(Dir.glob("#{Rails.root}/#{DATASHARE_CONFIG['uploads_dir']}"))
+    Dir.mkdir("#{Rails.root}/#{DATASHARE_CONFIG['uploads_dir']}")
+    Dir.mkdir("#{Rails.root}/#{DATASHARE_CONFIG['uploads_dir']}/1234567890")
+    FileUtils.touch("#{Rails.root}/#{DATASHARE_CONFIG['uploads_dir']}/1234567890/datacite.xml")
+    
+    Capybara.current_session.driver.header 'eppn', 'abc123'
+    
+    visit '/login'
+  end
+  
+  teardown do
+    FileUtils.rm_rf(Dir.glob("#{Rails.root}/#{DATASHARE_CONFIG['uploads_dir']}"))
+  end
+
+  # broken test, need to follow javascript redirect
+  test "navigate to upload page" do
+    visit "/record"
+    click_button 'Save And Continue'  
+    #assert page.has_content?('Upload your datasets to DataShare')
+  end
+  
+  # visit the page and validate text
+  # file upload functionality is covered in the upload functional test
+  test "file upload page" do
+    id = records(:one).id
+    visit "/record/#{id}/uploads"
+    assert page.has_content?("Upload")  
+  end
+  
+  # visit upload page for no record
+  test "no record redirects to my datasets page" do
+     visit "/record/100/uploads"
+    assert page.has_content?("My Datasets")
+  end
+  
+  # visit record not owned by current user
+  test "no ownership of record redirects to my datasets page" do
+    id = records(:two).id
+    visit "/record/#{id}/uploads"
+    assert page.has_content?("My Datasets")
+  end
+  
+  # attempt to destroy non existent record
+  test "require ownership of related record to destroy upload" do
+    id = records(:two).id
+    upload_id = uploads(:two).id
+    visit "/record/#{id}/datauploads/#{upload_id}/delete"
+    assert page.has_content?("My Datasets")
+  end
+  
+  test "delete upload" do  
+    id = records(:three).id
+    visit "/record/#{id}/uploads"
+  
+    Dir.mkdir("#{Rails.root}/#{DATASHARE_CONFIG['uploads_dir']}/234567891")
+    FileUtils.touch("#{Rails.root}/#{DATASHARE_CONFIG['uploads_dir']}/234567891/datacite.xml")
+    
+    assert page.has_content?("datacite.xml")
+    first(:link, 'delete').click
+    assert !page.has_content?("datacite.xml")
+  end  
+  
+  
+  test "purge files" do
+    visit "/records"
+    prev_count = Upload.all.count
+    
+    visit "/purge_old_files"
+    assert_not_equal prev_count, Upload.all.count
+  end
+
+  
+end
